9e6bae210e73edc6ec1534ce60c688763762e20b
Alyssa Bulatek <alyssabulatek@yahoo.com>
2017-06-28 14:44:34 -0400

    Update amp_uvdist.py
    This is the final iteration of amp_uvdist.py before its overhaul.

--- amp_uvdist.py
+++ amp_uvdist.py
@@ -1,7 +1,8 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+
 def amp_uvdist(sbfile,lbfile,binsize):
-    from astropy.io import fits
-    import matplotlib.pyplot as plt
-    import numpy as np
     plt.clf()
     filenames = [sbfile,lbfile]
 
@@ -15,12 +16,12 @@ def amp_uvdist(sbfile,lbfile,binsize):
 
         vis = image[0].data['data']
         real = (vis[:,0,0,0,:,0,0] + vis[:,0,0,0,:,1,0])/2.
-#        real = np.mean(real,axis=1)
+        real = np.mean(real,axis=1)
         imag = (vis[:,0,0,0,:,0,1] + vis[:,0,0,0,:,1,1])/2.
-#       imag = np.mean(imag,axis=1)
+        imag = np.mean(imag,axis=1)
 
-#        amp = np.sqrt(real**2 + imag**2)
-#        amp = amp.squeeze()
+        amp = np.sqrt(real**2 + imag**2)
+        amp = amp.squeeze()
         uvdist = np.sqrt(u**2 + v**2)
 #        plt.plot(uvdist,amp,'.')
 
@@ -30,6 +31,11 @@ def amp_uvdist(sbfile,lbfile,binsize):
         newreal = []
         newimag = []
         newamp = []
+        rstdevs = []
+        istdevs = []
+        rNs = []
+        iNs = []
+        amperrs = []
         rangemin = np.amin(uvdist)
         rangemax = np.amax(uvdist)
 
@@ -50,6 +56,15 @@ def amp_uvdist(sbfile,lbfile,binsize):
             imagav = np.mean(imag2)
             newimag.append(imagav)
 
+            rstdev = np.std(real2)
+            istdev = np.std(imag2)
+
+            rN = len(real2)
+            iN = len(imag2)
+
+            amperr = np.sqrt((rstdev**2/rN)*(realav**2/(realav**2+imagav**2))+(istdev**2/iN)*(imagav**2/(realav**2+imagav**2)))
+            amperrs.append(amperr)
+
         newuvdist = np.asarray(newuvdist)
         newuvdist = newuvdist[~np.isnan(newuvdist)]
         r = np.asarray(newreal)
@@ -57,16 +72,14 @@ def amp_uvdist(sbfile,lbfile,binsize):
         i = np.asarray(newimag)
         i = i[~np.isnan(i)]
         newamp = np.sqrt(r**2 + i**2)
-        rstdev = np.std(r)
-        istdev = np.std(i)
-        rN = len(r)
-        iN = len(i)
+        amperrs = np.asarray(amperrs)
+        amperrs = amperrs[~np.isnan(amperrs)]
 
-        amperr = np.sqrt((rstdev**2/rN)*(r**2/(r**2+i**2))+(istdev**2/iN)*(i**2/(r**2+i**2)))
-#        plt.plot(newuvdist,newamp,'.')
-        plt.errorbar(newuvdist,newamp,yerr=amperr,fmt='.')
+        plt.plot(newuvdist,newamp,'.')
+        plt.errorbar(newuvdist,newamp,yerr=amperrs,fmt='.')
         plt.xlabel('Distance from center of uv-plane (klambda)')
         plt.ylabel('Amplitude (Jy)')
         plt.title('Amplitude versus uv-distance')
         plt.grid(True)
         plt.show()
+
